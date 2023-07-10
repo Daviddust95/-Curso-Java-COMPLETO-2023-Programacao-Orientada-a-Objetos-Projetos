7bd7658739850e6f484e14b29a1025fd0217df03
Alisson David de Melo <daviddust4@hotmail.com>
2023-07-10 09:50:35 -0300

    Create ex001.java

---
+++ hello world/ex001.java
@@ -0,0 +1,11 @@
+// Escreva um programa que após executado, mostra o seguinte resultado "Hello World !"
+
+public class Main {
+
+
+public static void main(String[] args) {
+    // código a ser executado
+    System.out.println ("Hello World");
+    }
+
+}
