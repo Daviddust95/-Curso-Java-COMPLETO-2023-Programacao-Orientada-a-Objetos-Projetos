01d4cc0de5495f8b772eeb065ed504ef6bba0341
Alisson David de Melo <daviddust4@hotmail.com>
2023-07-11 12:30:31 -0300

    Update aula25.java

--- aula 25/aula25.java
+++ aula 25/aula25.java
@@ -3,19 +3,19 @@ import java.util.Scanner;
 public class aula25 {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner (System.in);
+        Scanner sc = new Scanner(System.in);
 
-            String x;
-            x = sc.next ();
-            System.out.println("Você digitou: " + x);
+        String x;
+        x = sc.next();
+        System.out.println("Você digitou: " + x);
 
-            sc.close();
-    
-    
-        /* Para fazer entrada de dados, nós vamos
+        sc.close();
+
+        /*
+         * Para fazer entrada de dados, nós vamos
          * criar um objeto do tipo "Scanner" da seguinte forma:
          * Scarnner sc = new Scanner (System.in);
-         * precisa informar no começo do programa 
+         * precisa informar no começo do programa
          * import java.util.Scanner;
          * fazer sc.close() quado não precisar mais
          * do objeto sc
@@ -24,5 +24,5 @@ public class aula25 {
          * x = sc.next();
          */
 
-}
+    }
 }
