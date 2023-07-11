f892bb1c74759fddcca12fb646929c5aad9ec16a
Alisson David de Melo <daviddust4@hotmail.com>
2023-07-11 14:06:39 -0300

    update

--- Aula 23/ex001.java
+++
@@ -1,8 +0,0 @@
-// Escreva um programa que após executado, mostra o seguinte resultado "Hello World !"
-public class ex001 {
-public static void main(String[] args) {
-    // código a ser executado
-    System.out.println ("Hello World !");
-    }
-
-}
---
+++ Aula_23/ex001.java
@@ -0,0 +1,8 @@
+// Escreva um programa que após executado, mostra o seguinte resultado "Hello World !"
+public class ex001 {
+    public static void main(String[] args) {
+        // código a ser executado
+        System.out.println("Hello World !");
+    }
+
+}
