f153658ac534e45ae979a173706c73bfa0a7eef1
Alisson David de Melo <daviddust4@hotmail.com>
2023-07-10 23:58:00 -0300

    Update aula24.java

--- aula 24/aula24.java
+++ aula 24/aula24.java
@@ -6,3 +6,28 @@ Sintaxe:
 1) A expressão é calculada;
 2) O resultado da expressão é armazenado na variável.
 */
+
+/* Exemplo 1
+ */
+
+public class aula24 {
+    public static void main(String[] args) {
+
+        /*int x, y;
+
+        x = 5;
+
+        y = 2* x;*/
+
+        int x;
+        double y;
+
+        x = 5;
+
+        y = 2* x;
+
+        System.out.println (x);
+        System.out.println(y);
+    }
+
+}
