287480f591edef4b47f5711bcd3ceab6b43be0bb
Alisson David de Melo <daviddust4@hotmail.com>
2023-07-11 00:44:46 -0300

    Update aula24.java

--- aula 24/aula24.java
+++ aula 24/aula24.java
@@ -13,12 +13,15 @@ Sintaxe:
 public class aula24 {
     public static void main(String[] args) {
 
-        /*int x, y;
+        /*
+        Exemplo 1
+        int x, y;
 
         x = 5;
 
-        y = 2* x;*/
+        y = 2* x;
 
+        Exemplo 2
         int x;
         double y;
 
@@ -26,8 +29,25 @@ public class aula24 {
 
         y = 2* x;
 
+        Exemplo 3
+        double b,B,h, area;
+        b = 6.0;
+        B = 8.0;
+        h = 5.0;
+        */
+
+        b = 6.0;
+        B = 8.0;
+        h = 5.0;
+
+
+        area = (b + B) /2.0 * h;
+        System.out.println(area);
+
+        /*
         System.out.println (x);
         System.out.println(y);
-    }
+       */
+}
 
 }
