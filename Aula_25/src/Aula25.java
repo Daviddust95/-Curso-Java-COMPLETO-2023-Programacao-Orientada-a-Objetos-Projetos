543bb24168a46c2b5652951844ff876fd9d18a79
Alisson David de Melo <daviddust4@hotmail.com>
2023-07-18 15:42:16 -0300

    Programar é vida ❤️

--- Aula_25/src/Aula25.java
+++ Aula_25/src/Aula25.java
@@ -4,16 +4,18 @@ import java.util.Scanner;
 public class aula25 {
     public static void main(String[] args) {
 
-        Locale.set.Defalt(Locale.US)
-        Scanner sc = new Scanner(System.in);
-
-        double x;
-        x = sc.nextDouble();
-        System.out.printf("Você digitou: " + x);
-
-        sc.close();
-
         /*
+         * Locale.set.Default(Locale.US);
+         * Scanner sc = new Scanner(System.in);
+         * 
+         * Para Ler vários dados na mesma linha
+         * 
+         * double x;
+         * x = sc.nextDouble();
+         * System.out.printf("Você digitou: " + x);
+         * 
+         * sc.close();
+         * 
          * Para fazer entrada de dados, nós vamos
          * criar um objeto do tipo "Scanner" da seguinte forma:
          * Scarnner sc = new Scanner (System.in);
@@ -24,7 +26,31 @@ public class aula25 {
          * suponha uma variável tipo String declarada:
          * String x;
          * x = sc.next();
+         * 
+         * Para ler um caractere
+         * 
+         * Suponha uma variável tipo char declarada:
+         * 
+         * char x;
+         * 
+         * x = sc.nex().charAT(0)
          */
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        String x;
+        int y;
+        double z;
+        x = sc.next();
+        y = sc.nextInt();
+        z = sc.nexDouble();
+        System.out.println("Dados digitados");
+
+        System.out.println(x);
+        System.out.println(y);
+        system.out.println(z);
+        sc.close();
 
     }
 }
