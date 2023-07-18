00bb8f55924b7114f4679409234585faa0815fbd
Alisson David de Melo <daviddust4@hotmail.com>
2023-07-18 09:08:40 -0300

    Atualização
    Deixei de usar o eclipse, e comecei a utilizar o vs code.

---
+++ Aula_24/src/App.java
@@ -0,0 +1,70 @@
+/*Processamento de dados 
+Comandos de atribuição.
+Sintaxe:
+
+<variável> = <expressão>;
+1) A expressão é calculada;
+2) O resultado da expressão é armazenado na variável.
+*/
+
+/* Exemplo 1
+ */
+
+public class aula24 {
+    public static void main(String[] args) {
+
+        /*
+         * Exemplo 1
+         * int x, y;
+         * 
+         * x = 5;
+         * 
+         * y = 2* x;
+         * 
+         * Exemplo 2
+         * int x;
+         * double y;
+         * 
+         * x = 5;
+         * 
+         * y = 2* x;
+         * 
+         * Exemplo 3
+         * double b,B,h, area;
+         * b = 6.0;
+         * B = 8.0;
+         * h = 5.0;
+         * 
+         * b = 6.0;
+         * B = 8.0;
+         * h = 5.0;
+         * 
+         * 
+         * area = (b + B) /2.0 * h;
+         * System.out.println(area);
+         * 
+         * System.out.println (x);
+         * System.out.println(y);
+         * 
+         * Exemplo 4
+         * 
+         * a = 5;
+         * b = 2;
+         * 
+         * resultado = (double) a / b;
+         * 
+         * System.out.println (resultado);
+         * 
+         * Exemplo 5
+         * 
+         * double a;
+         * int b;
+         * 
+         * a = 5.0;
+         * b = a;
+         * 
+         * System.out.println(b);
+         */
+    }
+
+}
