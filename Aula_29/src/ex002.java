7dba94606222c88eabb100f3a27f2b8c259453db
Alisson David de Melo <daviddust4@hotmail.com>
2023-07-24 13:04:04 -0300

    Programar é vida ❤️
    Continuo usando o vs code.

--- Aula_29/src/ex002.java
+++ Aula_29/src/ex002.java
@@ -1,5 +1,21 @@
-public class  {
+import java.util.Scanner;
+
+public class ex002 {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        double raio, area;
+        final double PI = 3.14159;
+
+        // Entrada do raio do círculo
+        System.out.println("Qual o valor do raio do círculo ?");
+        raio = scanner.nextDouble();
+
+        // Cálculo da área do círculo
+        area = PI * raio * raio;
+
+        // Exibir o resultado
+
+        System.out.println("A = %.4f", area);
+
     }
 }
